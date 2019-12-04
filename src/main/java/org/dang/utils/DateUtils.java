1ab0d978fb8349c35b7b897e792fb9af48b6e804
dangub86 <32620829+dangub86@users.noreply.github.com>
2019-12-04 07:50:38 +0100

    Create DateUtils.java

---
+++ src/main/java/org/dang/utils/DateUtils.java
@@ -0,0 +1,99 @@
+package farmtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.time.LocalDate;
+
+@SpringBootApplication
+public class SpringBootVuejsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVuejsApplication.class, args);
+	}
+
+	public interface Computus {
+
+		/**
+		 * When does Easter happen in a given year?
+		 * @param year Which year do you want to know about Easter? <em>e.g.</em> 2016
+		 * @return the day of March. 1 = March 1st, 2 = March 2nd, 31 = March 31st, 32 = April 1st, 33 = April 2nd, et cetera.
+		 */
+		int getEaster(int year) throws IllegalArgumentException;
+	}
+
+	public static class IanTaylorComputus implements Computus {
+
+		public static final IanTaylorComputus INSTANCE = new IanTaylorComputus();
+
+		private IanTaylorComputus(){}
+
+		public int getEaster(final int year) {
+			if(year < 0){
+				throw new IllegalArgumentException("getEaster: year must be non-negative, but was called with year = " + year);
+			}
+
+			final int a = year % 19;
+			final int b = year >> 2;
+			final int c = b / 25 + 1;
+			final int d = (c * 3) >> 2;
+			int e = ((a * 19) - ((c * 8 + 5) / 25) + d + 15) % 30;
+			e += (29578 - a - e * 32) >> 10;
+			e -= ((year % 7) + b - d + e + 2) % 7;
+			return e;
+		}
+	}
+
+	public static abstract class Easter4JBase {
+
+		protected static int getEasterAsDayOfMarch(int year){
+			return IanTaylorComputus.INSTANCE.getEaster(year);
+		}
+
+	}
+
+	/**
+	 * Figure out, for a given year, when Easter occurs.
+	 * Returns JDK8 {@link java.time.LocalDate} instances.
+	 *
+	 * <h3>Usage</h3>
+	 * <pre>
+	 * Easter4J.{@link #getEaster(int) getEaster}(2016);</pre>
+	 *
+	 * @see IanTaylorComputus IanTaylorComputus for the underlying implementation
+	 */
+	public static class Easter4J extends Easter4JBase {
+
+		private Easter4J(){}
+
+		/**
+		 * Figure out, for a given year, when Easter occurs.
+		 *
+		 * <h3>Usage</h3>
+		 * <pre>
+		 * LocalDate easter = Easter4J.getEaster(2016);
+		 * assert easter.getMonth() == 3;
+		 * assert easter.getDayOfMonth() == 27;</pre>
+		 *
+		 * @param year Non-negative integer representing the Gregorian year <em>e.g.</em> <code>2016</code>.
+		 * @return JDK8 {@link java.time.LocalDate} for the day that Easter occurs on the given year
+		 * @throws IllegalArgumentException if {@code year} is negative
+		 */
+		public static LocalDate getEaster(final int year) {
+			final int dayOfMarch = getEasterAsDayOfMarch(year);
+			return LocalDate.of(year, 3, 1).plusDays(dayOfMarch - 1);
+		}
+	}
+
+	@Bean
+	public void testEaster() {
+		int year = 2050;
+		LocalDate easter = Easter4J.getEaster(year);
+		System.out.println("EASTER FOR " + year + " -> " + easter);
+	}
+}
